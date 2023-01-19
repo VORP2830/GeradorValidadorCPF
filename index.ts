aed35e0a860e8fdb51a5a0f084a37f25f4cbf361
Victor Osório <91508749+VORP2830@users.noreply.github.com>
2023-01-19 09:43:07 -0300

    Update index.ts
    Adicionando função para gerar um CPF valido

--- index.ts
+++ index.ts
@@ -18,5 +18,27 @@ function ValidarCPF(cpf: string){
     console.log(`O CPF ${cpf} é invalido!`);
     }
 }
+function GerarCPF(): string {
+  let cpf: string = '';
+  let numeros: number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  for (let i: number = 0; i < 9; i++) {
+    let index: number = Math.floor(Math.random() * numeros.length);
+    cpf += numeros[index];
+  }
+  let calcularDigitos: number = 0;
+  for (let c: number = 0; c < 9; c++) {
+    calcularDigitos += parseFloat(cpf[c]) * (c + 1);
+  }
+  let primeiroDigitoVerificador: number = calcularDigitos % 11;
+  calcularDigitos = 0;
+  cpf += primeiroDigitoVerificador.toString();
+  for (let c: number = 0; c < 10; c++) {
+    calcularDigitos += parseFloat(cpf[c]) * c;
+  }
+  let segundoDigitoVerificador: number = calcularDigitos % 11;
+  cpf += segundoDigitoVerificador.toString();
 
+  return cpf;
+}
+console.log(GerarCPF())
 ValidarCPF('');
